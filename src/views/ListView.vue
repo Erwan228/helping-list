54139396f2f94fc97b26a835f546fef90f95e526
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-08-01 14:49:10 +0200

    k

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,7 +1,7 @@
 <template>
     <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/list">To-Do</router-link>
+    <router-link to="/list">To-Do</router-link> |
     <router-link to="/mat">Mat</router-link>
   </nav>
 </template>
