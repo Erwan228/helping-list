194f0fa1bb72cde9c4c2293a8e5ea0170c0c8243
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-08-01 14:41:48 +0200

    no user seect

--- src/App.vue
+++ src/App.vue
@@ -27,4 +27,7 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 } */
+* {
+  user-select: none;
+}
 </style>
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -2,5 +2,29 @@
     <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/list">To-Do</router-link>
+    <router-link to="/mat">Mat</router-link>
   </nav>
 </template>
+<script>
+export default {
+    name: 'ListView',
+    components: {
+  
+    }
+  }
+</script>
+
+<style scoped>
+    nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #1c2732;
+}
+
+nav a.router-link-exact-active {
+  color: blue;
+}
+</style>
