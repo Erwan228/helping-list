ad446acb65b6926391145ab0d93698c85a99245d
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-08-02 11:09:41 +0200

    satt opp link til db

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -4,16 +4,32 @@
     <router-link to="/list">To-Do</router-link> |
     <router-link to="/mat">Mat</router-link>
   </nav>
+  <div class="list">
+    <h2>To do</h2>
+    <div class="toDoTasks">
+
+    </div>
+  </div>
 </template>
 <script>
 export default {
-    name: 'ListView',
-    components: {
-  
+    name: "ListView",
+    data() {
+      return{
+        tasks: [],
+      }
+    },
+    mounted(){
+      fetch('http://localhost:3000/tasks')
+      .then(res => res.json())
+      .then(data => this.tasks = data)
+      .catch(err => console.log(err.message))
     }
   }
 </script>
 
 <style scoped>
-
+.list {
+  border: solid blue 10px;
+}
 </style>
