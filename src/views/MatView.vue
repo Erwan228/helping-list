020a2e2e8db6027b9eed43e39f177e71446618ce
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-08-03 10:30:21 +0200

    sd

--- src/views/MatView.vue
+++ src/views/MatView.vue
@@ -4,4 +4,18 @@
     <router-link to="/list">To-Do</router-link> |
     <router-link to="/mat">Mat</router-link>
   </nav>
+  <div class="interface">
+
+  </div>
 </template>
+
+<script>
+export default {
+  name: "MatView",
+
+}
+</script>
+
+<style scoped>
+
+</style>
