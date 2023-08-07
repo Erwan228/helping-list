1a729a46f6ef46d6331f82b0e67fccf4e0547be8
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-08-07 13:25:04 +0200

    sdf

--- src/views/matviews/MiddagView.vue
+++ src/views/matviews/MiddagView.vue
@@ -1,12 +1,32 @@
 <template>
-    <div class="dinnerList">
-        
+    <div class="oppskriftsListe">
+        <div v-if="oppskrifter.length">
+            <div v-for="oppskirft in oppskrifter">
+                <p>{{ oppskirft.rett }}</p>
+            </div>
+        </div>
+        <div v-else>
+            <p>Loading...</p>
+        </div>
     </div>
+    
 </template>
 
 <script>
+
 export default {
-    name: 'MiddagView'
+    name: 'MiddagView',
+    data(){
+        return{
+            oppskrifter: []
+        }
+    },
+    mounted(){
+        fetch('http://localhost:3000/oppskrifter')
+        .then(res => res.json())
+        .then(data => this.oppskrifter = data)
+        .catch(err => console.log(err.message))
+    }
 }
 </script>
 
